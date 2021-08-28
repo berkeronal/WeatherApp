fabdc5e8d5a17fca02ed4d0bc751bc13480ec97f
Berker <berker435@gmail.com>
2021-08-28 17:46:42 +0300

    Things fixed in Day Screen; Code refactored for clean code and abstraction. Memory leak caused by binding fixed. Responsibility issues fixed.

--- app/src/main/java/com/kekod/weatherapp/DayActivity.kt
+++ app/src/main/java/com/kekod/weatherapp/DayActivity.kt
@@ -5,64 +5,110 @@ import android.os.Bundle
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import androidx.core.view.isVisible
+import com.kekod.weatherapp.abstracts.CustomAnimationListener
 import com.kekod.weatherapp.databinding.ActivityDayBinding
 import com.kekod.weatherapp.databinding.ActivityMainBinding
 
-private lateinit var binding: ActivityDayBinding
+//notlar
+//-----------
+//generic function refied for onclicklistener
+//binding on destroy null : check
+//bi abstract class açıp onaimationlistener da boş olanları onun içine tanı , böylece boş overrride göz önünde olmasın : check
+//scope functions binding ler için. (with{binding}{}) : check
+//onanimationlistener için sağ sol vs hepsini farklı fonksionlara böl : check
+//complar arası bias : check
+
+
+//https://developer.android.com/topic/libraries/view-binding#fragments
+private var _binding: ActivityDayBinding? = null
+private val binding get() = _binding!!
+
 class DayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityDayBinding.inflate(layoutInflater)
+
+        _binding = ActivityDayBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
         //init anims
         val sunUp: Animation = AnimationUtils.loadAnimation(this, R.anim.to_up)
         val sunZoom: Animation = AnimationUtils.loadAnimation(this, R.anim.zoom_in)
+        //generice çek tekrardan kaçın
         val fadeIn: Animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
-        val leftToCenter: Animation = AnimationUtils.loadAnimation(this, R.anim.left_to_center)
 
 
 
-        sunUp.setAnimationListener(object: Animation.AnimationListener {
-            override fun onAnimationStart(animation: Animation?) {
-
-            }
-
+        sunUp.setAnimationListener(object : CustomAnimationListener() {
             override fun onAnimationEnd(animation: Animation?) {
-                binding.sun.startAnimation(sunZoom)
-                binding.location.startAnimation(fadeIn)
-                binding.location.isVisible = true
 
-                //right
-                binding.tempratureSVG.startAnimation(fadeIn)
-                binding.tempratureText.startAnimation(fadeIn)
-                binding.tempratureSVG.isVisible = true
-                binding.tempratureText.isVisible = true
+
+                //animate top
+                with(binding) {
+                    sun.startAnimation(sunZoom)
+                    tvLocation.startAnimation(fadeIn)
+                    tvLocation.isVisible = true
+                }
+
 
                 //left
-                binding.humidityText.startAnimation(leftToCenter)
-                binding.humiditySVG.startAnimation(leftToCenter)
-                binding.weatherText.startAnimation(leftToCenter)
-                binding.weatherSVG.startAnimation(leftToCenter)
-                binding.rainText.startAnimation(leftToCenter)
-                binding.rainSVG.startAnimation(leftToCenter)
-                binding.humidityText.isVisible = true
-                binding.humiditySVG.isVisible = true
-                binding.weatherText.isVisible = true
-                binding.weatherSVG.isVisible = true
-                binding.rainSVG.isVisible = true
-                binding.rainText.isVisible = true
-            }
+                animateBottomLeft()
 
-            override fun onAnimationRepeat(animation: Animation?) {
+                //right
+                animateBottomRight()
 
             }
+
         })
 
         binding.sun.startAnimation(sunUp)
 
 
+    }
 
+    fun animateTop() {
+
+    }
+
+    fun animateBottomLeft() {
+
+        //init anims
+        val leftToCenter: Animation = AnimationUtils.loadAnimation(this, R.anim.left_to_center)
+
+        with(binding) {
+            tvHumidity.startAnimation(leftToCenter)
+            ivHumiditySVG.startAnimation(leftToCenter)
+            tvWeather.startAnimation(leftToCenter)
+            ivWeatherSVG.startAnimation(leftToCenter)
+            tvRain.startAnimation(leftToCenter)
+            ivRainSVG.startAnimation(leftToCenter)
+            tvHumidity.isVisible = true
+            ivHumiditySVG.isVisible = true
+            tvWeather.isVisible = true
+            ivWeatherSVG.isVisible = true
+            ivRainSVG.isVisible = true
+            tvRain.isVisible = true
+        }
+
+
+    }
+
+    fun animateBottomRight() {
+
+        //init anim
+        val fadeIn: Animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+
+        with(binding) {
+            tvTempratureSVG.startAnimation(fadeIn)
+            tvTemprature.startAnimation(fadeIn)
+            tvTempratureSVG.isVisible = true
+            tvTemprature.isVisible = true
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/kekod/weatherapp/abstracts/CustomAnimationListener.kt
@@ -0,0 +1,15 @@
+package com.kekod.weatherapp.abstracts
+
+import android.view.animation.Animation
+
+abstract class CustomAnimationListener : Animation.AnimationListener{
+    override fun onAnimationStart(animation: Animation?) {
+
+    }
+
+
+    override fun onAnimationRepeat(animation: Animation?) {
+
+    }
+
+}
